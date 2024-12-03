d6691cd1f77c4781f1236bebe6a1d3532e962d6f
Abdurashidbek Sadullayev <135204006+iosproger@users.noreply.github.com>
2024-12-03 19:58:36 +0500

    Update dependencies.py

--- project/dependencies.py
+++ project/dependencies.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from app.database.database import SessionLocal
+from project.app.database.database import SessionLocal
 
 def get_db():
     db: Session = SessionLocal()
